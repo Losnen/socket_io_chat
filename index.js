31a48819fe8302eee5647b29686349e6fc25e065
Samuel Ramos <alu0100815146@ull.edu.es>
2016-10-07 17:17:58 +0100

    Añadiendo motor de vistas

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const path = require('path');
+var expressLayouts = require('express-ejs-layouts');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.render('index');
 });
 
 io.on('connection', function(socket){
