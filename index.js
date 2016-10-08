e0a80e2904dd2d450913598e05afbd6042712649
Samuel Ramos <alu0100815146@ull.edu.es>
2016-10-08 18:56:34 +0100

    Añadiendo passport para el login en la sala de chat

--- index.js
+++ index.js
@@ -4,23 +4,82 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 const path = require('path');
 var expressLayouts = require('express-ejs-layouts');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session')
+
+require('./config/passport')(passport); // pass passport for configuration
+
+mongoose.connect('mongodb://localhost/data'); // connect to our database
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(expressLayouts);
-
 app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-app.get('/', function(req, res){
-  res.render('index');
+app.get('/', function(req, res) {
+  res.render('index.ejs'); // load the index.ejs file
 });
+
+app.get('/chat', function(req, res){
+  res.render('chat');
+});
+
+
+app.get('/login', function(req, res) {
+	res.render('login', { message: req.flash('loginMessage') });
+});
+
+app.post('/login', passport.authenticate('local-login', {
+	successRedirect : '/profile', // redirect to the secure profile section
+	failureRedirect : '/login', // redirect back to the signup page if there is an error
+	failureFlash : true // allow flash messages
+}));
+
+app.get('/signup', function(req, res) {
+	res.render('signup', { message: req.flash('signupMessage') });
+});
+
+app.post('/signup', passport.authenticate('local-signup', {
+	successRedirect : '/profile', // redirect to the secure profile section
+	failureRedirect : '/signup', // redirect back to the signup page if there is an error
+	failureFlash : true // allow flash messages
+}));
+
+app.get('/profile', isLoggedIn, function(req, res) {
+	res.render('profile.ejs', {
+		user : req.user
+    });
+});
+
+app.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated())
+		return next();
+	res.redirect('/');
+}
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
-
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
